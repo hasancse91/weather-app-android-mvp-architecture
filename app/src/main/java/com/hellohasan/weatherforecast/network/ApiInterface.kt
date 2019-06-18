aa04814076e234d9740c6ea6e606a342c7be3e51
Hasan Abdullah <hasan.cse91@gmail.com>
2019-06-18 17:52:52 +0600

    api interface added

--- app/src/main/java/com/hellohasan/weatherforecast/network/ApiInterface.kt
+++ app/src/main/java/com/hellohasan/weatherforecast/network/ApiInterface.kt
@@ -1,2 +1,9 @@
 package com.hellohasan.weatherforecast.network
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiInterface {
+    @GET("/weather")
+    fun callApiForWeatherInfo(@Query("id") cityId: Int, @Query("appid") appId: String)
+}
