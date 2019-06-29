b9907b6ddaed5afedfca646b79c7978b83776397
Hasan Abdullah <hasan.cse91@gmail.com>
2019-06-29 23:48:16 +0600

    update

--- app/src/main/java/com/hellohasan/weatherforecast/Config.kt
+++ app/src/main/java/com/hellohasan/weatherforecast/Config.kt
@@ -3,6 +3,6 @@ package com.hellohasan.weatherforecast
 class Config {
     companion object {
         const val BASE_URL = "http://api.openweathermap.org/data/2.5/"
-        const val APP_ID = "d450a4a574372bd12f2fa308bf3cf15a"
+        const val APP_ID = "YOUR_OWN_APP_ID"
     }
 }
