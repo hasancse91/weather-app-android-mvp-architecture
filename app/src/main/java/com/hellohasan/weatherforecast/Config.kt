b4c9410195b62884ef5503dab1a86c74179d26cb
Hasan Abdullah <hasan.cse91@gmail.com>
2019-06-30 00:09:11 +0600

    config file updated

--- app/src/main/java/com/hellohasan/weatherforecast/Config.kt
+++ app/src/main/java/com/hellohasan/weatherforecast/Config.kt
@@ -2,7 +2,11 @@ package com.hellohasan.weatherforecast
 
 class Config {
     companion object {
-        const val BASE_URL = "http://api.openweathermap.org/data/2.5/"
-        const val APP_ID = "YOUR_OWN_APP_ID"
+        // if you use sample base url, the App ID should be the sample one
+        // if you use the real base URL your App ID must be a real App ID
+        // Note that, after creation of your App ID, it may need a few hours to activate
+        
+        const val BASE_URL = "http://api.openweathermap.org/data/2.5/" //sample base url: https://samples.openweathermap.org/data/2.5/
+        const val APP_ID = "YOUR_OWN_APP_ID" //sample App ID: b6907d289e10d714a6e88b30761fae22
     }
 }
